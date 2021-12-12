d91c7354dcc0a80e52c8543355bd08f188e937fa
Daniel Aarón Pipa Aguirre <45998749+DanielPipaAguirre@users.noreply.github.com>
2021-12-12 18:15:16 -0500

    pug handlebar jsx

--- index.js
+++ index.js
@@ -1,50 +1,30 @@
 const express = require("express");
 const path = require("path");
-const { Router } = express;
-const Contenedor = require("./contenedor");
+const handlebars = require("express-handlebars");
+const routerProduct = require("./components/products");
 
 const app = express();
 const PORT = 8080;
-const manejadorArchivo = new Contenedor();
-const router = new Router();
 
 app.get("/", async (req, res, next) => {
-    res.render("index.pug");
-});
-
-router.get("/", async (req, res, next) => {
-    const products = await manejadorArchivo.getAll();
-    // res.json(products);
-    res.render("table.pug", { products });
-});
-
-router.get("/:id", async (req, res, next) => {
-    const product = await manejadorArchivo.getById(+req.params.id);
-    if (product.length) return res.json(product);
-    res.json({ error: "producto no encontrado" });
-});
-
-router.post("/", async (req, res, next) => {
-    await manejadorArchivo.save(req.body);
-    res.redirect("/");
-});
-
-router.put("/:id", async (req, res, next) => {
-    await manejadorArchivo.updateById(+req.params.id, req.body);
-    res.json(req.body);
-});
-
-router.delete("/productos/:id", async (req, res, next) => {
-    await manejadorArchivo.deleteById(+req.params.id);
-    res.send(`Producto ${req.params.id} eliminado`);
+    res.render("formulario.hbs");
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/productos", router);
-/* app.use("/public", express.static(path.join(__dirname, "public"))); */
-app.set("views", path.join(__dirname, "views", "pug"));
-app.set("view engine", "pug");
+app.use("/api/productos", routerProduct);
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.engine(
+    "hbs",
+    handlebars.create({
+        extname: ".hbs",
+        defaultLayout: "index.hbs",
+        layoutsDir: __dirname + "/views/hbs",
+        partialsDir: __dirname + "/views/hbs",
+    }).engine
+);
+app.set("views", path.join(__dirname, "views", "hbs"));
+app.set("view engine", "hbs");
 
 const server = app.listen(PORT, () => {
     console.log(
